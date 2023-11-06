49142042cafca7838be67c8f4bff6d599af9e2f9
root <root@ip-172-31-44-98.ap-south-1.compute.internal>
2023-11-06 06:02:24 +0000

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,1 @@
-# Define your Docker image name and tag
-IMAGE_NAME="reactjs-demo"
-TAG="v1"
-
-# Define the address of your server
-SERVER_ADDRESS="13.233.112.82"
-
-# Push the Docker image to a Docker registry (if needed)
-# Example:
-# docker push your-registry-url/$IMAGE_NAME:$TAG
-
-# SSH into the server and pull the Docker image
-ssh user@$SERVER_ADDRESS <<EOF
-docker pull $IMAGE_NAME:$TAG
-docker run -d -p 80:3000 --name $IMAGE_NAME $IMAGE_NAME:$TAG
-EOF
+docker-compose up -d
